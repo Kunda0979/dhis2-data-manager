cf8e12f81852a6f6d20667e5fc9c8ed9ad85d210
Oliver Kunda <Kundaoliver1@gmail.com>
2026-08-18 14:39:07 +0000

    Reset stale browser state for DataBridge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,18 @@ import App from './App.jsx'
 import RuntimeErrorBoundary from './components/common/RuntimeErrorBoundary.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+    .catch(() => undefined)
+}
+
+if ('caches' in window) {
+  window.caches.keys()
+    .then((keys) => Promise.all(keys.map((key) => window.caches.delete(key))))
+    .catch(() => undefined)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RuntimeErrorBoundary>
