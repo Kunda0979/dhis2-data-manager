9ebb83a95094297824ddd100acff0c44e73d574b
Oliver Kunda <Kundaoliver1@gmail.com>
2026-08-18 14:52:35 +0000

    Restore stable frontend delivery

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,18 +4,6 @@ import App from './App.jsx'
 import RuntimeErrorBoundary from './components/common/RuntimeErrorBoundary.jsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations()
-    .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
-    .catch(() => undefined)
-}
-
-if ('caches' in window) {
-  window.caches.keys()
-    .then((keys) => Promise.all(keys.map((key) => window.caches.delete(key))))
-    .catch(() => undefined)
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RuntimeErrorBoundary>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  build: {
-    assetsDir: 'databridge-assets',
-  },
   server: {
     host: '0.0.0.0',
     port: 3000,
